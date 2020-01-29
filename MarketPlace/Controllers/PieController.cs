3a71972b7a4fa8eaee1fff457cb01dda4de06eaf
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-01-29 13:23:37 +0000

    Add navigation to Pie details

--- MarketPlace/Controllers/PieController.cs
+++ MarketPlace/Controllers/PieController.cs
@@ -26,5 +26,17 @@ namespace MarketPlace.Controllers
             //return View(_pieRepository.Pies);
             return View(piesListViewModel);
         }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+
+            if (pie == null)
+            {
+                return NotFound();
+            }
+
+            return View(pie);
+        }
     }
 }
