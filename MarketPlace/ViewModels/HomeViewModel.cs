9ac61a2e12666dd5069bc6f9cd23cd819f585227
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-01-29 13:44:06 +0000

    working with partial views

---
+++ MarketPlace/ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using MarketPlace.Models;
+using System.Collections.Generic;
+
+namespace MarketPlace.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Pie> PiesOfTheWeek { get; set; }
+    }
+}
