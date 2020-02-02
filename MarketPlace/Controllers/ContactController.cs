e83b79736971eed4ddf57b76d35cf576ad7384ff
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-02-02 10:07:33 +0000

    Customize TagHelper

---
+++ MarketPlace/Controllers/ContactController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MarketPlace.Controllers
+{
+    public class ContactController : Controller
+    {
+        [ResponseCache(Duration = 30, Location = ResponseCacheLocation.Client)]
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
